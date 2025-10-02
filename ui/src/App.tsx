f7b2d49f6ea4095c4cf21f015f8ac578dba355a6
Davina Leong <leong.shi.yun@gmail.com>
2025-10-02 17:39:53 +0800

    Added a Static Table to the projects page

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,13 @@
 // import Login from "./pages/Auth/Login"
 // import Privacy from "./pages/Documents/Privacy"
 // import Terms from "./pages/Documents/Terms"
-import Dashboard from "./pages/Modules/Dashboard"
+// import Dashboard from "./pages/Modules/Dashboard"
+import Projects from "./pages/Modules/Projects"
 // import Test from "./pages/Modules/Test"
 import "./App.css"
 
 function App() {
-  return <Dashboard />
+  return <Projects />
 }
 
 export default App

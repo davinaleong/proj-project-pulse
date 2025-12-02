3cdbf5a23350b13b52e5e552204acdf3a95ad3fb
Davina Leong <leong.shi.yun@gmail.com>
2025-12-02 22:40:54 +0800

    Removed the deprecated Next.js project

--- _dep-ai/src/app/components/AzureAiSearchLink.tsx
+++
@@ -1,5 +0,0 @@
-import Link from 'next/link'
-
-export default function AzureAiSearchLink() {
-    return <Link href="https://azure.microsoft.com/en-us/services/cognitive-search/" className="anchor" target="_blank">Azure AI Search</Link>
-}
--- _dep-ai/src/app/components/AzureOpenaiLink.tsx
+++
@@ -1,5 +0,0 @@
-import Link from 'next/link'
-
-export default function AzureOpenaiLink() {
-    return <Link href="https://azure.microsoft.com/en-us/services/cognitive-services/openai-service/" className="anchor" target="_blank">Azure OpenAI</Link>
-}

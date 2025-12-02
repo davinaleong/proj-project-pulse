9f12b2f6d5045e5964f872fac990718b43141bad
Davina Leong <leong.shi.yun@gmail.com>
2025-12-02 18:24:47 +0800

    Created a Main component

---
+++ ai/src/app/components/Main.tsx
@@ -0,0 +1,22 @@
+import Header from './Header'
+import Footer from './Footer'
+import Container from "./Container"
+
+interface MainProps {
+  children: React.ReactNode;
+  className?: string
+}
+
+export default function Main({ children, className="" }: MainProps) {
+  return (
+    <div className="font-sans bg-ppai-slate-50 text-ppai-slate-900">
+      <Container className="min-h-screen py-4 h-full flex flex-col">
+        <Header />
+
+        <main className={`flex-1 flex items-center justify-center ${className}`}>{children}</main>
+
+        <Footer />
+      </Container>
+    </div>
+  )
+}
--- ai/src/app/page.tsx
+++ ai/src/app/page.tsx
@@ -1,6 +1,4 @@
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Container from "./components/Container"
+import Main from './components/Main'
 
 import AzureAiSearchLink from './components/AzureAiSearchLink'
 import AzureOpenaiLink from './components/AzureOpenaiLink'
@@ -10,26 +8,18 @@ import StaticButton from './components/StaticButton'
 
 export default function Home() {
   return (
-    <div className="font-sans bg-ppai-slate-50 text-ppai-slate-900">
-      <Container className="min-h-screen py-4 h-full flex flex-col">
-        <Header />
+    <Main>
+      <div className="flow">
+        <h2 className="text-center text-2xl font-bold">Welcome to Project Pulse AI</h2>
 
-        <div className="flex-1 flex items-center justify-center">
-          <div className="flow">
-            <h2 className="text-center text-2xl font-bold">Welcome to Project Pulse AI</h2>
-
-            <p className="text-center">A personal learning project built to showcase my skills in AI, data analytics, and dashboard design.<br/>
+        <p className="text-center">A personal learning project built to showcase my skills in AI, data analytics, and dashboard design.<br/>
 Powered by <AzureAiSearchLink /> and <AzureOpenaiLink />, this prototype demonstrates what I’ve learned during my BCG Business & Data Analytics course.</p>
 
-            <Nav justifyContent="center">
-              <StaticButton href="/chatbot" bgColor="bg-ppai-teal-500">AI Chatbot</StaticButton>
-              <StaticButton href="/dashboard" bgColor="bg-ppai-teal-500">Interactive Dashboard</StaticButton>
-           </Nav>
-          </div>
-        </div>
-
-        <Footer />
-      </Container>
-    </div>
+        <Nav justifyContent="center">
+          <StaticButton href="/chatbot" bgColor="bg-ppai-teal-500">AI Chatbot</StaticButton>
+          <StaticButton href="/dashboard" bgColor="bg-ppai-teal-500">Interactive Dashboard</StaticButton>
+        </Nav>
+      </div>
+    </Main>
   );
 }

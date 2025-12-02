6201109b69646e6fd5dcf9bfa3b595db08af1835
Davina Leong <leong.shi.yun@gmail.com>
2025-12-02 18:31:06 +0800

    Made the text larger

--- ai/src/app/dashboard/page.tsx
+++ ai/src/app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import Main from './../components/Main'
 export default function Dashboard() {
   return (
     <Main className="border border-gray-500 bg-gray-300">
-      <p className="text-center text-gray-500 uppercase">Coming Soon</p>
+      <p className="text-center text-gray-500 text-4xl uppercase">Coming Soon</p>
     </Main>
   );
 }

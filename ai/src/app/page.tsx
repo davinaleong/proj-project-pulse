bd0d70642d2f22437cb72369bc999a0e2dabfdef
Davina Leong <leong.shi.yun@gmail.com>
2025-12-02 17:23:50 +0800

    Created a Container component

--- ai/src/app/page.tsx
+++ ai/src/app/page.tsx
@@ -1,21 +1,24 @@
 import Link from 'next/link';
 import Header from './components/Header'
+import Container from "./components/Container"
 
 export default function Home() {
   return (
-    <div className="font-sans bg-ppai-slate-50 text-ppai-slate-900 min-h-screen p-4">
-      <Header />
+    <div className="font-sans bg-ppai-slate-50 text-ppai-slate-900 min-h-screen py-4">
+      <Container>
+        <Header />
       
-      <nav>
-        <ul>
-          <li>
-            <Link href="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link href="/chatbot">Chatbot</Link>
-          </li>
-        </ul>
-      </nav>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link href="/chatbot">Chatbot</Link>
+            </li>
+          </ul>
+        </nav>
+      </Container>
     </div>
   );
 }
